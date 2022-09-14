5dd882c78ad893f30f0af7f501ee92717bab29e9
serverBKD <serverbkd@outlook.com>
2022-09-14 10:33:15 -0400

    now14/09 Finalizar index.html + tienda.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        copy: "#612066",
+        copy: "#873CB3",
         bg: "#212139",
         bg0: "#9DB4C0",
         card: "#F7D002",
@@ -21,16 +21,28 @@ module.exports = {
         mandarin: "#F4743B",
         gunmetal: "#292F36",
         erieBlack: "#141414",
+        pal0: "#2274A5",
+      },
+      aspectRatio: {
+        "4/3": "4/3",
+        "9/16": "9/16",
+        "3/4": "3/4",
       },
       fontFamily: {
         sans: ["Open Sans", "Ubuntu", "sans-serif"],
+      },
+      fontSize: {
+        xxs: ".5rem",
+      },
+      maxWidth: {
+        "1/2": "50%",
       },
       gridTemplateColumns: {
         // Simple 12 column grid
         12: "repeat(12, minmax(120px, 1fr))",
 
         // Complex site-specific column configuration
-        footer: "200px minmax(900px, 1fr) 100px",
+        tienda: "500px minmax(300px, 1fr) 160px",
       },
     },
   },
